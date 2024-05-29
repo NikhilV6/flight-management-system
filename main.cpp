1e54245220d518d28efb294b0b375ff64d6ec72f
NikhilV6 <nikhilverma17133@gmail.com>
2024-05-29 19:56:04 +0530

    project completed

--- main.cpp
+++ main.cpp
@@ -1,104 +1,15 @@
 #include<iostream>
 #include<fstream>
-#include <iomanip>
-
+#include<iomanip>
 
 using namespace std;
-
 void mainMenu();
-
-
 class Managment{ 
     public:
         Managment(){
             mainMenu();
         }
 };
-
-void mainMenu(){
-    int lchoice;
-    int schouce;
-    int back;
-
-    cout<<"\t\t\tXYZ Airlines \n"<<endl;
-    cout<<"\t____________Main Menu____________"<<endl;
-    cout<<"\t__________________________________"<<endl;
-    cout<<"\t\t\t\t\t\t"<<endl;
-    cout<<"\t\t Press 1 to add the Customer Details    \t"<<endl;
-    cout<<"\t\t Press 2 for Flight Registration        \t"<<endl;
-    cout<<"\t\t Press 3 for Ticket and Charges    \t"<<endl;
-    cout<<"\t\t Press 4 to exit                   \t"<<endl;
-    cout<<"\t_______________________________________________"<<endl; 
-
-    cout<<"Enter the choice : ";
-    cin>>lchoice;
-
-    Details d;
-    registration r;
-    ticket t;
-
-    switch(lchoice){
-        case 1:
-        {
-            cout<<"_____________Customers_____________\n"<<endl;
-           d.information();
-            cout<<"Press any key to go back to Main menu";
-            cin>>back;
-            if(back == 1){
-                mainMenu();
-
-            }
-            else{
-                mainMenu();
-            }
-            break;
-        }
-        case 2:
-        {   
-            cout<<"______________Book a flight using this system______________\n"<<endl;
-            r.flight();
-            break;
-        }
-        
-        case 3:{
-            cout<<"____CET YOUR TICKET______\n"<<endl;
-           t.bill();
-            cout<<"Your ticket is printed, you can collet it \n"<<endl;
-            cout<<"Press 1 to display your ticket ";
-            cin>>back;
-            if(back == 1){
-                t.display();
-                cout<<"Press any key to go back to main menu:";
-                cin>>back;
-                if(back == 1){
-                    mainMenu();
-                }
-                else{
-                    mainMenu();
-                }
-            
-
-            }
-            else{
-                mainMenu();
-            }
-            
-
-             break;
-             }
-       
-        case 4:{
-            cout<<"\n\n\t_______Thank You_______"<<endl;
-            break;
-        }
-        
-        default:{
-            cout<<"Invalid Input"<<endl;
-            mainMenu();
-        }
-    }
-}
-
 
 
 class Details
@@ -123,6 +34,7 @@ class Details
             cout<<"\n Gender: ";
             cin>>gender;
             cout<<"\n Details Saved Sucessfully "<<endl;
+           
         }
 
 };
@@ -444,6 +356,130 @@ class registration
         }
 };
 
+
+float registration::charges;
+int registration::choice;
+
+class ticket : public registration , Details
+{
+    public:
+        void bill(){
+            string destination = "";
+            ofstream outf("records.txt");
+            {
+                outf<<"___________XYZ Airlines_______"<<endl;
+                outf<<"___________Ticket_____________"<<endl;
+                outf<<"______________________________"<<endl;
+
+                outf<<"Customer ID: "<<Details::cId<<endl;
+                outf<<"Customer Name: "<<Details::name<<endl;
+                outf<<"Customer Gender: "<<Details::gender<<endl;
+                outf<<"\tDescription\n"<<endl;
+
+                if(registration::choice == 1)destination = "Dubai";
+                else if(registration::choice == 2)destination = "Canada";
+                else if(registration::choice == 3)destination = "UK";
+                else if(registration::choice == 4)destination = "USA";
+                else if(registration::choice == 5)destination = "Australia";
+                else if(registration::choice == 6)destination = "Europe";
+
+
+                outf<<"Destination\t\t"<<destination<<endl;
+                outf<<"Flight cost :\t\t"<<registration::charges<<endl;
+            }
+            outf.close(); 
+        }
+        void display(){
+            ifstream ifs("records.txt");
+            {
+                if(!ifs)cout<<"file Error"<<endl;
+                while(!ifs.eof()){
+                    ifs.getline(arr,100);
+                    cout<<arr<<endl;
+                }
+            }
+            ifs.close();
+        }
+};
+
+void mainMenu(){
+    int lchoice;
+    int schouce;
+    int back;
+
+    cout<<"\t\t\tXYZ Airlines \n"<<endl;
+    cout<<"\t____________Main Menu____________"<<endl;
+    cout<<"\t__________________________________"<<endl;
+    cout<<"\t\t\t\t\t\t"<<endl;
+    cout<<"\t\t Press 1 to add the Customer Details    \t"<<endl;
+    cout<<"\t\t Press 2 for Flight Registration        \t"<<endl;
+    cout<<"\t\t Press 3 for Ticket and Charges    \t"<<endl;
+    cout<<"\t\t Press 4 to exit                   \t"<<endl;
+    cout<<"\t_______________________________________________"<<endl; 
+
+    cout<<"Enter the choice : ";
+    cin>>lchoice;
+
+    Details d;
+    registration r;
+    ticket t;
+
+    switch(lchoice){
+        case 1:
+        {
+            cout<<"_____________Customers_____________\n"<<endl;
+           d.information();
+            cout<<"Press any key to go back to Main menu";
+            cin>>back;
+            
+                mainMenu();
+            
+            break;
+        }
+        case 2:
+        {   
+            cout<<"______________Book a flight using this system______________\n"<<endl;
+            r.flight();
+            break;
+        }
+        
+        case 3:{
+            cout<<"____CET YOUR TICKET______\n"<<endl;
+           t.bill();
+            cout<<"Your ticket is printed, you can collect it \n"<<endl;
+            cout<<"Press 1 to display your ticket ";
+            cin>>back;
+            if(back == 1){
+                t.display();
+                cout<<"Press any key to go back to main menu:";
+                cin>>back;
+                mainMenu();
+            
+
+            }
+            else{
+                mainMenu();
+            }
+            
+
+             break;
+             }
+       
+        case 4:{
+            cout<<"\n\n\t_______Thank You_______"<<endl;
+            return;
+        }
+        
+        default:{
+            cout<<"Invalid Input"<<endl;
+            mainMenu();
+        }
+    }
+}
+
+
+
 int main(){
-    return 0 ;
+    Managment m;
+    return 0;
 }
